61924ec2c9df43affc3d1b54dff53664bea2ccf9
Patrick McDavid <patrick@bombbomb.com>
2015-02-04 11:30:24 -0700

    Jasmine?

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,25 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: '<%= pkg.name %>.js',
+                src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        jasmine: {
+            customTemplate: {
+                src: 'src/**/*.js',
+                options: {
+                    specs: 'spec/*Spec.js',
+                    helpers: 'spec/*Helper.js'
+                }
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
-
+    grunt.registerTask('default', ['uglify', 'jasmine']);
 };
