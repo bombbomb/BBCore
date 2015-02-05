2ce324723abda39d4e9e1c3f21fb86b198b0d3f0
Patrick McDavid <pmcdavid@gmail.com>
2015-02-04 22:56:26 -0700

    moved test config into spec/

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         },
         karma: {
             unit: {
-                configFile: 'karma.conf.js',
+                configFile: 'spec/karma.conf.js',
                 singleRun: true
             }
         }
--- spec/karma.conf.js
+++ spec/karma.conf.js
@@ -2,10 +2,10 @@ module.exports = function(config) {
     config.set({
         frameworks: ['jasmine'],
         files: [
-            'src/jquery.js',
-            'src/*.js',
-            'spec/*.js',
-            { pattern: 'src/jquery.js', watched: false, included: true }
+            '../src/jquery.js',
+            '../src/*.js',
+            '*.js',
+            { pattern: '../src/jquery.js', watched: false, included: true }
         ],
         reporters: ['dots'],
         browsers: ['PhantomJS']
